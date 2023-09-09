babac4557db9aac3e4458d1909e2f3217a10216f
Shirerpeton <hyloxalusfallax@gmail.com>
2023-09-09 20:00:04 +0400

    add some configs

---
+++ .config/nvim/init.lua
@@ -0,0 +1,4 @@
+require("hylo.set")
+require("hylo.remap")
+require("hylo.theme")
+require("hylo")
---
+++ .config/nvim/lua/hylo/remap.lua
@@ -0,0 +1,17 @@
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>fv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("v", "<leader>p", "\"+p")
+vim.keymap.set("n", "<leader>p", "\"+P")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+
+vim.keymap.set("n", "<C-b>", "<C-a>")
