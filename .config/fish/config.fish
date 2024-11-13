7c894e1b689ee886b871711b21f110f95b992f7f
Shirerpeton <hyloxalusfallax@gmail.com>
2024-11-13 15:03:53 +0400

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,32 +1,63 @@
 function fish_greeting
 end
 
-function last_history_item
-  echo $history[1]
+function fish_user_key_bindings
+    fish_vi_key_bindings
+    bind -M insert \cf  '~/.local/scripts/tms.sh'
+    bind -M insert \cn '~/.local/scripts/tms.sh ~/ misc'
+    #bind -M insert \cn 'cd (fd . -t d | fzf)'
+    bind -M insert \es 'fish_commandline_prepend doas'
 end
 
-function fish_user_key_bindings
-  fish_vi_key_bindings
-  bind -M insert \cf '~/.local/scripts/tms.sh'
-  bind -M insert \cn '~/.local/scripts/tms.sh ~/ misc'
+function __fish_prepend_sudo
 end
+
+function last_history_item
+    echo $history[1]
+end
+
+nvm use --silent latest
 
 if status is-interactive
-  alias ls='eza'
-  alias grep='grep --color=auto'
-  alias sudo='doas'
-  alias ks='keepassxc-cli open ~/pswd/Passwords.kdbx'
+    alias ls 'eza'
+    #alias grep 'grep --color=auto'
+    alias ks 'keepassxc-cli open ~/pswd/Passwords.kdbx'
+    alias sudo "doas"
 
-  abbr --add sudo doas
-  abbr --add n nvim
-  abbr --add d doas
-  abbr --add sctl --position anywhere systemctl
-  abbr --add pc --position anywhere pacman
-  abbr --add !! --position anywhere --function last_history_item
+    alias jj 'j (fd . -t d | fzf)'
+    alias jh 'j (fd . '/home/hylo/' -t d | fzf)'
 
-  set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
-  set -x PATH "$PATH:/home/hylo/.local/bin"
+    abbr --add sudo doas
+    abbr --add d doas
+    abbr --add pc --position anywhere pacman
+    abbr --add n --position anywhere nvim
+    abbr --add !! --position anywhere --function last_history_item
+    abbr --add ddcd doas docker compose down
+    abbr --add ddcdv doas docker compose down -v
+    abbr --add ddcb doas docker compose build
+    abbr --add ddcu doas docker compose up
 
-  starship init fish | source
-  zoxide init fish --cmd j | source
+    abbr --add nivm nvim
+
+    abbr --add find fd
+    abbr --add grep rg
+    abbr --add curl xh
+
+
+    #ssh
+    set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+    set -x NODE_EXTRA_CA_CERTS "/etc/ca-certificates/trust-source/anchors/corp-ca.crt"
+    ssh-add ~/.ssh/gitlab 2> /dev/null 
+
+    #source /usr/share/nvm/init-nvm.sh
+
+    starship init fish | source
+    zoxide init fish --cmd j | source
+
+    # bun
+    set BUN_INSTALL "$HOME/.bun"
+    set PATH $BUN_INSTALL/bin:$PATH
+
+    #pipx
+    set PATH "$PATH:/home/hylo/.local/bin"
 end
