98d6616db95e4c55ffe34cdc27ba5dbd1b891d83
Shirerpeton <hyloxalusfallax@gmail.com>
2024-12-12 00:33:05 +0400

    add highlight on yank in neovim config

--- .config/nvim/lua/hylo/init.lua
+++ .config/nvim/lua/hylo/init.lua
@@ -17,3 +17,11 @@ function ToggleBufferAutoreload()
 end
 
 vim.api.nvim_create_user_command('ToggleAutoreload', ToggleBufferAutoreload, {})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+    desc = 'Highlight when yanking text',
+    group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end
+})
