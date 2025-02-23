cf637007a8a82df0c3a9e7ed7270a4ae26458346
Shirerpeton <hyloxalusfallax@gmail.com>
2025-02-24 01:22:36 +0400

    add script for rebuilding aur pkgs

---
+++ misc/scripts/rebuild-paru.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+paru -Syua --rebuild hyprcursor-git hyprgraphics-git hyprland-qtutils-git hyprland-git hyprlang-git hyprlock-git hyprpaper-git waybar-git hyprutils-git aquamarine-git hyprland-protocols-git hyprsunset-git hyprwayland-scanner-git
