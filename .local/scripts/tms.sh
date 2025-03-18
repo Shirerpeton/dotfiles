d346b9aa2afdd98185f9bcb95a2c426f78d47258
Shirerpeton <hyloxalusfallax@gmail.com>
2025-03-18 18:00:40 +0400

    update tms script

--- .local/scripts/tms.sh
+++ .local/scripts/tms.sh
@@ -3,17 +3,25 @@
 if [[ $# -eq 1 ]] || [[ $# -eq 2 ]]; then
     dir=$1
 else
-    dir=$(find ~/ ~/misc ~/projects ~/projects/zig ~/projects/angular -mindepth 1 -maxdepth 1 -type d | fzf)
+    dir=$(fd -d 1 -t d '' ~/ ~/misc ~/repos ~/qemu ~/repos/*.git/branches | fzf --tmux)
 fi
 
-name=$(echo $dir | awk -F'/' '{
-    result = ""
-    for(i = 2; i < NF; i++) {
-        result = result "/" substr($i, 1, 1)
-    }
-    result = result "/" $NF
-    print result
-}')
+if [[ $dir == *".git/"* ]]; then
+    name=$(echo $dir | awk '
+        BEGIN { RS = "/" };
+        /\.git$/{result = $0}
+        NF { last = $0 }
+        END{print(result " - " last)}' | tr . _) 
+else
+    name=$(echo $dir | awk -F'/' '{
+        result = ""
+        for(i = 2; i < NF; i++) {
+            result = result "/" substr($i, 1, 1)
+        }
+        result = result "/" $NF
+        print result
+    }' | tr . _)
+fi
 
 if [[ $# -eq 2 ]]; then
     name=$2
@@ -26,18 +34,17 @@ fi
 tmux_pid=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_pid ]]; then
-    tmux new-session -s $name -c $dir
+    tmux new-session -s "$name" -c $dir
     exit 0
 fi
 
 
-if ! tmux has-session -t=$name 2> /dev/null; then
-    tmux new-session -ds $name -c $dir
+if ! tmux has-session -t="$name" 2> /dev/null; then
+    tmux new-session -ds "$name" -c $dir
 fi
 
 if [[ -n $TMUX ]]; then
-    tmux switch -t $name
+    tmux switch -t "$name"
 else
-    tmux attach-session -t $name
+    tmux attach-session -t "$name"
 fi
-
